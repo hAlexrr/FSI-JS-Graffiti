17d01c22f9110dd487c197e44c662549923e4fa6
Alex Rosales <rosalesa512@gmail.com>
2022-04-21 18:48:34 -0700

    Page formatted changed using JavaScript

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+
+let dogImages = document.querySelectorAll('.dog-image')
+for( let img of dogImages){
+    img.style.borderRadius = '50px';
+    img.style.transform = 'rotate(180deg)';
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+
+for ( let name of dogNames ){
+    name.style.textAlign = 'left';
+}
+
+let footer = document.querySelector('.footer')
+
+footer.style.color = 'blue'
